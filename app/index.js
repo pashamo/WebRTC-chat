90e6fcee54e41c05eeaafce949faef9d089dbbac
Mohammed Pasha <mk.pasha@hotmail.com>
2021-10-01 07:43:32 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+// const socket = new WebSocket('ws://localhost:8000');
+
+// socket.onmessage = ({data}) => {
+//     console.log(`Message received by server ${data}`);
+//     printData(data);
+// };
+
+// document.querySelector('#buhhum').onclick = () => {
+//     console.log('Button clicked');
+//     socket.send('10-4');
+// };
+
+// printData = data => {
+//     let messages = document.querySelector('#messages');
+//     let newLI = document.createElement('li');
+//     let content = document.createTextNode(data);
+//     newLI.appendChild(content);
+//     messages.appendChild(newLI);
+// };
---
+++ server/server.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({ port: 8000 });
+
+// server.on("connection", socket => {
+
+//     socket.on("message", message => {
+//         socket.send(`Message received from client: ${message}`);
+//     });
+
+// });
