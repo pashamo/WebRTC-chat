6c65f9cb2896fda0265d400212df68bde8429b60
Mohammed Pasha <mk.pasha@hotmail.com>
2021-10-01 08:57:52 -0400

    feat: Socket.io multi user chat implemented

--- app/index.js
+++ app/index.js
@@ -1,10 +1,21 @@
+const socket = io('ws://localhost:8000');
+
+socket.on("message", message => {
+    let messages = document.querySelector('#messages');
+    let newLI = document.createElement('li');
+    let content = document.createTextNode(message);
+    newLI.appendChild(content);
+    messages.appendChild(newLI);
+});
+
+document.querySelector("#buhhum").onclick = () => {
+    let message = document.querySelector('#message-input').value;
+    socket.emit('message', message);
+}
 
 
 
-
-
-
-
+//Vanilla WebSocket example
 // const socket = new WebSocket('ws://localhost:8000');
 
 // socket.onmessage = ({data}) => {
--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+io.on("connection", socket => {
+    console.log(`${socket.id} has connected`);
+
+    socket.on("message", message => {
+        console.log(message);
+        io.emit('message', `${socket.id} said ${message}`);
+    })
+})
+
+http.listen(8000, () => console.log('listening on port 8000'));
 
 
-
-
-
+//Vanilla WebSocket example
 // const WebSocket = require('ws');
 // const server = new WebSocket.Server({ port: 8000 });
 
